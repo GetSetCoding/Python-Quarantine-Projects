12929466bea5182036c784baf12ab8eece9ad712
mikyas zeyede <70920410+nihilo-mike@users.noreply.github.com>
2021-11-11 23:10:09 +0800

    Added suggestions for errors
    it will print out suggestions for the errors along with the error position and errors

--- SpellChecker/spellchecker.py
+++ SpellChecker/spellchecker.py
@@ -1,12 +1,13 @@
+import enchant
 from enchant.checker import SpellChecker 
 from enchant.tokenize import EmailFilter,URLFilter
+dictionary = enchant.request_dict("en_US")
 spell=SpellChecker("en-US",filters=[EmailFilter,URLFilter])     
 filepath=str(input('Enter file location'))
 fileopen=open(filepath)
-#print(fileopen.read())        
 content=fileopen.read()
 spell.set_text(content)
 for err in spell:
-        print("Misspelled word is: "+err.word+' at position '+ str(err.wordpos))
-        
+        print("Misspelled word is: "+err.word+' at position '+ str(err.wordpos)+" "
+        +"suggestions"+str(dictionary.suggest(err.word)))  
           
